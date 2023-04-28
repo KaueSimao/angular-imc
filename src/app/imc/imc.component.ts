3602b2f7f22e1efd1976b314091278cab09255fe
kauesimao <kauesimao1234@gmail.com>
2023-04-28 14:27:26 -0300

    final

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -10,12 +10,27 @@ export class ImcComponent {
   peso : number ;
   altura : number;
   imc : number;
+  resul : string;
 
 cal(){
-    this.imc = this.peso / (this.altura * 2);
+    this.imc = this.peso / (this.altura * this.altura);
+    if (this.imc   <= 18.4)
+    this.resul= 'Magreza';
 
-     this.imc;
+    if  (this.imc >= 18.5 )
+    this.resul= 'Peso Ideal';
 
+    if  (this.imc >= 24.9 )
+    this.resul= 'Acima do Peso';
+
+    if  (this.imc >= 29.9 )
+    this.resul= 'Obesidade Grau I';
+
+    if  (this.imc >= 34.9 )
+    this.resul= 'Obesidade Grau II';
+
+    if  (this.imc >= 39.9 )
+    this.resul= 'Obesidade Grau III';
   }
 
 }
